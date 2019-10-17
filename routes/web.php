6ff768bb096f0362ceb77a6e721f7eafcbe0d0fb
Luis Esteban <luisesteban.cch@gmail.com>
2019-10-16 23:47:24 -0500

    deploy initial

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+use Illuminate\Support\Str;
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->get('/key', function(){
+    return Str::random(32);
+});
+
+$router->get('/users', ['uses' => 'UsersController@index']);
